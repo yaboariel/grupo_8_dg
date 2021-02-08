52a85f5a819c8d6c2f9c683246730352a498ccef
santiquin <75916039+santiquin@users.noreply.github.com>
2021-02-08 19:27:35 -0300

    Login + Register
    Logo es solo un boceto, pie de pagina a modo de ilustrativo

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+app.set('puerto', process.env.PORT || 3000);
+
+app.listen (app.get('puerto'), ()=> console.log(`Servidor corriendo de manera satisfactoria  ${app.get('puerto')}` ));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve((__dirname, './views/home.html')))
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve((__dirname, './views/register.html')))
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve((__dirname, './views/login.html')))
+});
