1a945e42a2ac8bd74e5a4c9d5cd08075aff789dc
AgusR-99 <76019584+AgusR-99@users.noreply.github.com>
2021-02-08 22:34:10 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 const webRouter = require('./routes/webRouter');
+
 app.use('/',webRouter);
 
+app.get("/productDetail", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+})
+
+app.get("/productCart", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
+})
+
+app.get("/login", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
+
+app.get("/register", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+
 app.listen('3001', () => console.log('Servidor esta corriendo en el puerto 3001'));
+
